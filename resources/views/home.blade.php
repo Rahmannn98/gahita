a8a6e9ebbaac5c760f561f473055a290da13e7f5
Saifulloh Rahman <saifulloh.rahman028@student.unud.ac.id>
2025-03-20 06:46:39 +0800

    refactor: replace 'artikel' section with 'start' section and remove unused artikel file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
     @include('sections.support')
     @include('sections.lifetime')
     @include('sections.testimoni')
-    @include('sections.artikel')
+    @include('sections.start')
 
     <script src="{{ asset('js/script.js') }}"></script>
     <x-footer></x-footer>
---
+++ resources/views/sections/start.blade.php
@@ -0,0 +1,16 @@
+<div class="py-24 bg-white dark:bg-gray-900 sm:py-32">
+  <section class="bg-white dark:bg-gray-900">
+    <div class="items-center max-w-screen-xl gap-8 px-4 py-8 mx-auto xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+        <img class="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image">
+        <img class="hidden w-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image">
+        <div class="mt-4 md:mt-0">
+            <h2 class="mb-4 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">Ayo Mulai Pembelajaran Anda bersama kami</h2>
+            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Gahita membantu anda untuk bertemu dan belajar dengan orang yang memiliki ketertarikan yang sama dengan seni dan budaya bali</p>
+            <a href="/dashboard" class="inline-flex items-center text-blue-500 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+                Get started
+                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+        </div>
+    </div>
+</section>
+</div>
